338e819f94acedb5b3d2321972f9271722d1907c
zadigg <62830716+zadigg@users.noreply.github.com>
2021-09-17 18:40:48 +0300

    final update

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,27 +1,30 @@
-import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
-import { forwardRef } from "react";
+import Image from "next/image";
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+function Thumbnails({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
-
   return (
     <div
-      ref={ref}
-      className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+      className="group p-2 cursor-pointer transition duration-100 
+            ease-in transform sm-hover:scale-115 hover:z-50"
     >
       <Image
         layout="responsive"
-        src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`
+        }
         height={1080}
         width={1920}
       />
 
-      <div className="p-2">
+      <div>
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+
+        <h2 className=" mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
           {result.title || result.original_name}
         </h2>
+
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} .`}{" "}
           {result.release_date || result.first_air_date} .{" "}
@@ -30,6 +33,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       </div>
     </div>
   );
-});
+}
 
-export default Thumbnail;
+export default Thumbnails;
